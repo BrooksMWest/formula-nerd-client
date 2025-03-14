e8d756171e2c25df1a5c1b32a73b3beeef222ec8
Brooks West <brooksmwest@hotmail.com>
2025-03-14 17:18:27 -0500

    got past google auth now working on register

---
+++ pages/circuits.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Circuits() {
+  return <div>Circuits</div>;
+}
---
+++ pages/races.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Races() {
+  return <div>Races</div>;
+}
